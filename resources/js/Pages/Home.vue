abd66c48f8ed1ece02d38b669af2cd81e1802ee0
faisal4590 <optimizedfaisal42@gmail.com>
2025-04-22 00:39:42 +0600

    laravel vue boilerplate

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="min-h-screen bg-gray-100">
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <div class="px-4 py-6 sm:px-0">
+        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96 p-4">
+          <h1 class="text-4xl font-bold text-red-900 mb-4">
+            Welcome to your Inertia.js + Vue.js app!
+          </h1>
+          <p class="text-lg text-gray-600">
+            This page is styled with Tailwind CSS
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
